b877ee134a6a7d3d26bf278b28f8763b98b62266
Mateus Vahl <mateus.vahl@possible.com>
2018-10-11 17:19:34 +0000

    add base structure folder

---
+++ src/structure/header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+import logo from './logo.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <img src={logo} alt="front-end-brasil-logo"></img>
+      </header>
+    )
+  }
+}
+
+export default Header;
