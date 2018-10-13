fcf97cbe87f4a548b77f62bd00027fa2a543237c
Mateus Vahl <mateus.vahl@possible.com>
2018-10-13 03:22:43 +0000

    add days format

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,24 +1,35 @@
+import timeStrFromDate from '../../helpers/timeStrFromDate'
 import React, { Component } from 'react'
 import Label from '../label';
 import './index.scss';
 
 class Card extends Component {
 
+  date(d) {
+    const date = new Date(d)
+    return timeStrFromDate(date);
+  }
   render() {
     const { card } = this.props;
+    const date = this.date(card.created_at);
     const hasLabels = Boolean(card.labels.length);
-    const labels = card.labels.map((_, i) => {
-      return (<Label label={_} key={i}></Label>)
-    })
+    const labels = card.labels.map((_, i) => (<Label label={_} key={i}></Label>))
 
     return (<div className="card">
       <div className="card__content">
-        <a className="card__user" href={card.user.html_url} target="_blank" rel="noopener noreferrer">
-          <img className="card__user__photo" src={card.user.avatar_url} alt={card.user.login} />
-          <p className="card__user__name">{card.user.login}</p>
-        </a>
+
+
+        <div className="card__header">
+          <a className="card__user" href={card.user.html_url} target="_blank" rel="noopener noreferrer">
+            <img className="card__user__photo" src={card.user.avatar_url} alt={card.user.login} />
+            <p className="card__user__name">{card.user.login}</p>
+          </a>
+
+          <time className="card__time">{date}</time>
+        </div>
 
         <h1>{card.title}</h1>
+
 
         {hasLabels &&
           (<div className="card__labels">
--- src/i18n.js
+++ src/i18n.js
@@ -3,5 +3,16 @@ export default {
     search: {
       placeholder: 'Pesquisar'
     }
+  },
+
+  date: {
+    now: 'agora',
+    minutes: '$ atrás',
+    oneHour: '1 hora atrás',
+    hours: `$ horas atrás`,
+    yesterday: 'ontem',
+    days: `$ dias atrás`,
+    week: '1 semana atrás',
+    weeks: '$ semanas atrás'
   }
 }
